5c2b0fd4b6e9756fe066e3e996b9172f9f171946
G4bbi4 <agabbiani99@gmail.com>
2026-04-23 12:09:27 +0200

    WIP: funzia

--- main.py
+++ main.py
@@ -85,6 +85,37 @@ class Grid:
             print(" ".join(row))
     
         return grid
+        
+    #dovrebbe andare bene qui MA FORSE E' RIDONDANTE CON QUELLA SOPRA
+    def populate(self, agents_config, objects_config):
+        """
+        Function that populates the grid with agents and objects based on the provided configurations.
+        :param grid: the grid to be populated
+        :param agents_config: a list of tuples, where each tuple contains the id, x and y coordinates of an agent
+        :param objects_config: a list of tuples, where each tuple contains the x and y coordinates of an object
+        """
+        for agent_id, x, y in agents_config:
+            agent = Agent(agent_id)
+            agent.set_position(x, y, self) #con self passo la griglia
+            self.add_agent(agent)
+        
+        for x, y in objects_config:
+            obj = Object((x, y))
+            self.add_object(obj)
+
+    def reset_grid(self):
+        """
+        Resets the grid to its initial state.
+        """
+
+        # Reset agents to start positions
+        for i, (name, x, y) in enumerate(AGENTS):
+            self.agents[i].position = (x, y)
+        
+        # Reset objects picked status
+        for obj in self.objects:
+            obj.set_picked(False)
+
 
 class Agent:
 
@@ -98,16 +129,17 @@ class Agent:
         self.id = id
         self.position = None
 
-    def set_position(self, x, y):
+    def set_position(self, x, y, grid):
         """
         Sets the position of the agent. 
         The method checks if the new position is valid (not a wall and within grid boundaries) before updating the agent's position.
 
+        :param grid: the grid instance
         :param x: x coordinate of the agent's position
         :param y: y coordinate of the agent's position
         """
 
-        if (x, y) in g.walls or x < 0 or x >= g.size or y < 0 or y >= g.size:
+        if (x, y) in grid.walls or x < 0 or x >= grid.size or y < 0 or y >= grid.size:
             return  # Instead of raising error, stay in the same position
         else:
             self.position = (x, y)
@@ -135,7 +167,7 @@ class Agent:
         if (new_x, new_y) in grid.walls or new_x < 0 or new_x >= grid.size or new_y < 0 or new_y >= grid.size:
             return (False, (new_x, new_y))  # Move failed, return False and the attempted position
         
-        self.set_position(new_x, new_y)
+        self.set_position(new_x, new_y, grid)
         return (True, (new_x, new_y))  # Move successful, return True and the new position
 
 class Object:
@@ -166,12 +198,15 @@ class Trainer:
     Centralized Trainer for Multi-Agent Q-learning.
     Manages a single joint Q-table for all agents.
     """
-    def __init__(self, grid, alpha=0.1, gamma=0.9, epsilon=0.1):
+    def __init__(self, grid, c1=1, c2=1, gamma=0.9):
         #riguarda i valori dei parametri
         self.grid = grid
-        self.alpha = alpha
+        self.t = 0 #numero di passi totali, usato per decrescere alpha nel tempo
+        self.c1 = c1
+        self.c2 = c2
+        self.alpha = self.c1 / (self.c2 + self.t)
         self.gamma = gamma
-        self.epsilon = epsilon
+        self.epsilon = 1
         # joint state:
         self.num_states = (grid.size ** 2) ** len(grid.agents)
         # joint actions:
@@ -191,8 +226,9 @@ class Trainer:
             pos_idx = y * self.grid.size + x
             coords.append(pos_idx) # avrò ad es: [19, 13]
 
-        shape = (self.grid.size ** 2,) * len(self.grid.agents) # 64x64
-        idx = int(np.ravel_multi_index(coords, shape)) #indice dell'elemento [19,13] della tabella 64x64
+        #shape = (self.grid.size ** 2,) * len(self.grid.agents) # 64x64
+        #idx = int(np.ravel_multi_index(coords, shape)) #indice dell'elemento [19,13] della tabella 64x64
+        idx = coords[0] + coords[1] * (self.grid.size ** 2)
         return idx
     
     def get_joint_action(self, action_idx):
@@ -211,12 +247,25 @@ class Trainer:
             :param state_idx: the index of the current state in the Q-table
             """
             if np.random.rand() < self.epsilon:
-                return np.random.randint(self.num_joint_actions)
-            return np.argmax(self.q_table[state_idx])
+                action = np.random.randint(self.num_joint_actions)
+
+                if self.t > 0:
+                    self.epsilon = 1 / self.t
+                
+                return action
+            
+            else:
+                action = np.argmax(self.q_table[state_idx])
+
+                if self.t > 0:
+                    self.epsilon = 1 / self.t
+
+                return action
 
     def step(self, joint_action_idx):
         """
         Execute joint action, handle object collection, and check goal.
+        :param joint_action_idx: the index of the joint action to be executed
         """
         actions = self.get_joint_action(joint_action_idx)
         total_reward = 0
@@ -224,21 +273,21 @@ class Trainer:
         
         # Apply actions
         for i, agent in enumerate(self.grid.agents):
-            outcome.append(agent.move(actions[i]))
+            outcome.append(agent.move(actions[i], self.grid))
 
         # --- DA METTERE REWARD MURI E REWARD CELLE VUOTE ---
         for agent, (success, attempted_position) in zip(self.grid.agents, outcome):
             if not success:  # se il movimento non è riuscito (muro o fuori dai limiti)
-                total_reward -= 1
+                total_reward -= 0.5
             else:
-                object_picked = False  # flag per verificare se è stato raccolto un oggetto in questo step
+                is_picked = False  # flag per verificare se è stato raccolto un oggetto in questo step
                 for obj in self.grid.objects:
                     if not obj.picked and agent.position == obj.position:
                         obj.set_picked(True)
                         total_reward += 10   # Reward per ogni oggetto raccolto
-                        object_picked = True
+                        is_picked = True
                         break
-                if not object_picked:
+                if not is_picked:
                     total_reward -= 0.04
 
         # Verifica se tutti gli oggetti sono stati raccolti
@@ -254,31 +303,36 @@ class Trainer:
         return total_reward, reached_goal
 
     def update(self, state, action, reward, next_state): #RICONTROLLA I NOMI DEI PARAMETRI IN INGRESSO
-            """Aggiorna la Q-table usando la formula di Q-learning.
+            """
+            Aggiorna la Q-table usando la formula di Q-learning.
             :param state: l'indice dello stato corrente
             :param action: l'indice dell'azione eseguita
             :param reward: la ricompensa ricevuta dopo aver eseguito l'azione
             :param next_state: l'indice dello stato successivo dopo aver eseguito l'azione
             """
+
+            self.t += 1
+            self.alpha = self.c1 / (self.c2 + self.t)
+
             best_next_action = np.argmax(self.q_table[next_state])
             estimated_q_opt = reward + self.gamma * self.q_table[next_state][best_next_action]
+
             # aggiornamento tabella Q
             self.q_table[state][action] += self.alpha * (estimated_q_opt - self.q_table[state][action])
 
-    def train(self, episodes=1000):
+    def train(self, episodes):
+            """
+            Esegue il processo di training per un numero specificato di episodi.
+            :param episodes: il numero di episodi di training da eseguire
+            """
             for ep in range(episodes):
-                # Reset agents to start positions (hardcoded for now)
-                for i, (name, x, y) in enumerate(AGENTS):
-                    self.grid.agents[i].position = (x, y)
-                
-                # Reset objects picked status
-                for obj in self.grid.objects:
-                    obj.set_picked(False)
-                
+
+                self.grid.reset_grid()  # Resetta la griglia all'inizio di ogni episodio    
+                 
                 state = self.get_state_index()
                 done = False
                 steps = 0
-                while not done and steps < 100:
+                while not done and steps < 100000:
                     action = self.select_action(state)
                     reward, done = self.step(action)
                     next_state = self.get_state_index()
@@ -286,65 +340,43 @@ class Trainer:
                     state = next_state
                     steps += 1
                 
-                if ep % 100 == 0:
-                    print(f"Episode {ep} finished in {steps} steps")
-
-#funzione a se stante (da rivedere se può essere inserita in una classe)
-def populate(grid, agents_config, objects_config):
-    """
-    Function that populates the grid with agents and objects based on the provided configurations.
-    :param grid: the grid to be populated
-    :param agents_config: a list of tuples, where each tuple contains the id, x and y coordinates of an agent
-    :param objects_config: a list of tuples, where each tuple contains the x and y coordinates of an object
-    """
-    for agent_id, x, y in agents_config:
-        agent = Agent(agent_id)
-        agent.set_position(x, y)
-        grid.add_agent(agent)
-    
-    for x, y in objects_config:
-        obj = Object((x, y))
-        grid.add_object(obj)
-
+                #self.grid.render()
+                print(f"Episode {ep} finished in {steps} steps, epsilon: {self.epsilon:.16f}")
 
 if __name__ == "__main__":
+
+    # max_demo_steps = 100
     g = Grid(8)
-    populate(g, AGENTS, OBJECTS)
+    g.populate(AGENTS, OBJECTS)
     print('Initial grid:')
     g.render()
 
-    trainer = Trainer(g)
-    print("Starting Centralized Training...")
-    trainer.train(episodes=501)
+    trainer = Trainer(g, c1=1, c2=1)
+    print(f"Starting Centralized Training with c1={trainer.c1}, c2={trainer.c2}...")
+    trainer.train(episodes = 350)
+    #g.render()
+    print(trainer.q_table)  # controlla se è tutta uguale/zero
+    print(f"Epsilon finale: {trainer.epsilon:.16f}")  # dovrebbe essere vicino a 0
+    
+    g.reset_grid()
 
- # Show final result
-    print("Final demo run:")
-    for i, (name, x, y) in enumerate(AGENTS):
-        g.agents[i].position = (x, y)
-    
-    state = trainer.get_state_index()
-    for _ in range(10):
-        os.system('clear')
-        g.render()
-        action = np.argmax(trainer.q_table[state])
-        trainer.step(action)
+    done = False
+    steps = 0
+    while not done and steps < 5000:
         state = trainer.get_state_index()
-        time.sleep(0.5)
-        if any(a.position == g.delivery_station for a in g.agents):
-            os.system('clear')
-            g.render()
-            print("Goal Reached!")
-            break
+        action = np.argmax(trainer.q_table[state]+ np.random.randn(trainer.num_joint_actions) * 1e-7)  # Aggiunta di rumore per evitare scelte sempre uguali in caso di valori Q simili
+        reward, done = trainer.step(action)
+        steps += 1
+        #os.system('clear')
+        g.render()
+        # print(trainer.q_table)  # controlla se è tutta uguale/zero
+        print(f"Epsilon finale: {trainer.epsilon:.16f}")  # dovrebbe essere vicino a 0
+        print(f"State: {state}")
+        print(f"Steps: {steps}")
+        #print(f"Q-values: {trainer.q_table[state]}")
+        time.sleep(0.15)
     
-    # Animazione: muovi l'agente A verso destra per 3 passi
-    for _ in range(5):
-        try:
-            g.agents[0].move("down")  # Muovi l'agente A a destra
-        except ValueError:
-            break  # Se non può muoversi, interrompi
-        
-        time.sleep(0.5)  # Ritardo di 0.5 secondi
-        os.system('clear')  # Cancella lo schermo (su Linux/Mac; usa 'cls' su Windows)
-        g.render()  # Ristampa la griglia aggiornata
-    
-    print("Animazione completata.")
+    if done:
+        print("Goal Reached!")
+    else:
+        print("Demo completed without reaching the goal.")
