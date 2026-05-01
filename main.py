f812f0093127df49f6dd57cd300de9ff36eefb65
G4bbi4 <agabbiani99@gmail.com>
2026-05-01 17:38:06 +0200

    WIP: convergence fixing

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.animation import FuncAnimation
+from matplotlib.animation import PillowWriter
+from matplotlib.animation import FFMpegWriter
 import numpy as np
 import time
 import os
+import sys
+
+class Tee:
+    def __init__(self, filename):
+        self.file = open(filename, 'w')
+        self.stdout = sys.stdout
+    def write(self, data):
+        self.stdout.write(data)
+        self.file.write(data)
+    def flush(self):
+        self.stdout.flush()
+        self.file.flush()
 
 AGENTS = [
     ("A", 1, 1),
@@ -103,6 +119,45 @@ class Grid:
             obj = Object((x, y))
             self.add_object(obj)
 
+    def plot_grid(self, fig = None, ax = None):
+        """
+        Plots the grid using patches for walls, station, objects, and agents.
+        """
+        if fig is None or ax is None:
+            fig, ax = plt.subplots(figsize = (8, 8))
+
+        # Set axis limits and appearance
+        ax.set_xlim(0, self.size )
+        ax.set_ylim(self.size, 0)
+        ax.set_aspect('equal')
+        ax.set_xticklabels([])   # rimuove etichette X
+        ax.set_yticklabels([])   # rimuove etichette Y
+        ax.grid(color = 'black', linewidth = 1)
+
+        # Plot walls
+        for (wx, wy) in self.walls:
+            ax.add_patch(mpatches.Rectangle((wx, wy), 1, 1, facecolor='#CE3018', edgecolor='white', hatch='//'))
+
+        # Plot delivery station
+        sx, sy = self.delivery_station
+        ax.plot(sx + 0.5, sy + 0.5, marker='*', markersize=40, markerfacecolor='green', markeredgecolor='black')
+
+        # Plot objects
+        for obj in self.objects:
+            if not obj.picked:
+                ox, oy = obj.position
+                ax.add_patch(mpatches.Circle((ox + 0.5, oy + 0.5), 0.3, facecolor='#03C03C', edgecolor='black'))
+            else:
+                ox, oy = obj.position
+                ax.plot(ox + 0.5, oy + 0.5, marker='X', markersize=35, markerfacecolor='#03C03C', markeredgecolor='black')
+        
+        # Plot agents
+        for agent in self.agents:
+            ax.add_patch(mpatches.Circle((agent.position[0] + 0.5, agent.position[1] + 0.5), 0.35, color='darkblue'))
+            ax.text(agent.position[0] + 0.5, agent.position[1] + 0.5, agent.id, color='white', ha='center', va='center', fontsize=12, weight='bold')
+
+        return fig, ax
+
     def reset_grid(self):
         """
         Resets the grid to its initial state.
@@ -198,7 +253,7 @@ class Trainer:
     Centralized Trainer for Multi-Agent Q-learning.
     Manages a single joint Q-table for all agents.
     """
-    def __init__(self, grid, c1=1, c2=1, gamma=0.9):
+    def __init__(self, grid, c1=1, c2=1, gamma=0.9): #PROVA A VARIARE GAMMA
         #riguarda i valori dei parametri
         self.grid = grid
         self.t = 0 #numero di passi totali, usato per decrescere alpha nel tempo
@@ -206,7 +261,7 @@ class Trainer:
         self.c2 = c2
         self.alpha = self.c1 / (self.c2 + self.t)
         self.gamma = gamma
-        self.epsilon = 1
+        self.epsilon = 0.1
         # joint state:
         self.num_states = (grid.size ** 2) ** len(grid.agents)
         # joint actions:
@@ -249,16 +304,20 @@ class Trainer:
             if np.random.rand() < self.epsilon:
                 action = np.random.randint(self.num_joint_actions)
 
-                if self.t > 0:
-                    self.epsilon = 1 / self.t
+                # if self.t > 0:
+                #     self.epsilon = 1 / self.t
                 
                 return action
             
             else:
-                action = np.argmax(self.q_table[state_idx])
+                #action = np.argmax(self.q_table[state_idx])
+                q_values = self.q_table[state_idx]
+                max_q = np.max(q_values)
+                best_actions = np.where(q_values == max_q)[0]
+                action = np.random.choice(best_actions)
 
-                if self.t > 0:
-                    self.epsilon = 1 / self.t
+                # if self.t > 0:
+                #     self.epsilon = 1 / self.t
 
                 return action
 
@@ -320,14 +379,17 @@ class Trainer:
             # aggiornamento tabella Q
             self.q_table[state][action] += self.alpha * (estimated_q_opt - self.q_table[state][action])
 
-    def train(self, episodes):
+    def train(self, episodes, decay_rate):
             """
             Esegue il processo di training per un numero specificato di episodi.
             :param episodes: il numero di episodi di training da eseguire
             """
             for ep in range(episodes):
 
-                self.grid.reset_grid()  # Resetta la griglia all'inizio di ogni episodio    
+                self.grid.reset_grid()  # Resetta la griglia all'inizio di ogni episodio
+                
+
+                self.epsilon = max(0.01, self.epsilon * decay_rate)
                  
                 state = self.get_state_index()
                 done = False
@@ -343,40 +405,110 @@ class Trainer:
                 #self.grid.render()
                 print(f"Episode {ep} finished in {steps} steps, epsilon: {self.epsilon:.16f}")
 
+    def animate_grid(self, interval=100, repeat=True, max_steps=5000):
+        """
+        Funzione che esegue una animazione attraverso FuncAnimation
+        """
+        fig, ax = plt.subplots(figsize=(5, 5))  # figura più piccola
+        self.grid.reset_grid()
+
+        done = False
+        steps = 0
+
+        def update(frame):
+            nonlocal done, steps
+            ax.clear()
+            # Primo frame: mostra solo lo stato iniziale, nessuna azione
+            if frame == 0:
+                self.grid.plot_grid(fig=fig, ax=ax)
+                return []
+            if done or steps >= max_steps:
+                if anim.event_source is not None:
+                    anim.event_source.stop()
+                return []
+            state = self.get_state_index()
+            action = np.argmax(self.q_table[state]) #+ np.random.randn(self.num_joint_actions) * 1e-7
+            reward, done = self.step(action)
+            steps += 1
+            self.grid.plot_grid(fig=fig, ax=ax)
+            return []
+
+        anim = FuncAnimation(fig=fig, func=update, frames=range(max_steps+1), interval=interval, repeat=repeat)
+        return anim
+
+
 if __name__ == "__main__":
 
-    # max_demo_steps = 100
+    sys.stdout = Tee("training_log.txt") 
+
+    # - - - HYPERPARAMETERS CONFIGURATION - - -
+    HP_C1 = 1
+    HP_C2 = 100
+    HP_GAMMA = 0.9
+    HP_EPISODES = 350
+    
     g = Grid(8)
     g.populate(AGENTS, OBJECTS)
+    g.objects[0].set_picked(True)
     print('Initial grid:')
+    # fig,ax =g.plot_grid()
+    # plt.show()
     g.render()
 
-    trainer = Trainer(g, c1=1, c2=1)
-    print(f"Starting Centralized Training with c1={trainer.c1}, c2={trainer.c2}...")
-    trainer.train(episodes = 350)
-    #g.render()
-    print(trainer.q_table)  # controlla se è tutta uguale/zero
-    print(f"Epsilon finale: {trainer.epsilon:.16f}")  # dovrebbe essere vicino a 0
+    # - - - TRAINING - - -
+
+    trainer = Trainer(g, c1=HP_C1, c2=HP_C2, gamma=HP_GAMMA)
+    print(f"Hyperparameters: c1={HP_C1}, c2={HP_C2}, gamma={HP_GAMMA}")
+    print(f"Starting Centralized Training with c1={trainer.c1}, c2={trainer.c2}, gamma={trainer.gamma}...")
+    trainer.train(episodes = HP_EPISODES, decay_rate=0.99)
+
+    # - - - Q-TABLE SAVE/LOAD - - -
+
+    np.save('q_table.npy', trainer.q_table)
+    print("Q-table salvata in q_table.npy")
     
+    #Per caricare in seguito:
+    #q_table = np.load('q_table.npy')
+
+    # - - - ANIMAZIONE - - -
+
+    #g.reset_grid()
+    #anim = trainer.animate_grid()
+    # Salva animazione come GIF (opzionale)
+    #anim.save('Q_learning_grid.gif', writer=PillowWriter(fps=2, bitrate=1000), dpi=100)
+
+    # Salva animazione come MP4 usando FFMpegWriter esplicito
+    #anim.save('Q_learning_grid.mp4', writer=FFMpegWriter(fps=5, bitrate=1000), dpi=100)
+    
+    #anim.save('prova.mp4', writer=FFMpegWriter(fps=5, bitrate=1000), dpi=100)
+
+    
+    # - - - DEMO TEST - - -
+
     g.reset_grid()
 
     done = False
     steps = 0
+    print("Starting demo ...")
     while not done and steps < 5000:
         state = trainer.get_state_index()
-        action = np.argmax(trainer.q_table[state]+ np.random.randn(trainer.num_joint_actions) * 1e-7)  # Aggiunta di rumore per evitare scelte sempre uguali in caso di valori Q simili
+        #action = np.argmax(trainer.q_table[state])# + np.random.randn(trainer.num_joint_actions) * 1e-7)  # Aggiunta di rumore per evitare scelte sempre uguali in caso di valori Q simili
+        q_values = trainer.q_table[state]
+        max_q = np.max(q_values)
+        best_actions = np.where(q_values == max_q)[0]
+        action = np.random.choice(best_actions)
         reward, done = trainer.step(action)
         steps += 1
         #os.system('clear')
-        g.render()
-        # print(trainer.q_table)  # controlla se è tutta uguale/zero
-        print(f"Epsilon finale: {trainer.epsilon:.16f}")  # dovrebbe essere vicino a 0
-        print(f"State: {state}")
-        print(f"Steps: {steps}")
-        #print(f"Q-values: {trainer.q_table[state]}")
-        time.sleep(0.15)
+        # g.render()
+        # print(f"Epsilon finale: {trainer.epsilon:.16f}")
+        # print(f"State: {state}")
+        # print(f"Steps: {steps}")
+        # time.sleep(0.20)
     
     if done:
-        print("Goal Reached!")
+        print(f"Goal Reached in {steps} steps!")
     else:
         print("Demo completed without reaching the goal.")
+
+    #RICORDARSI DI TOGLIERE RENDER E PRINT VARI 
